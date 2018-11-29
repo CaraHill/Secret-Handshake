ca90c0b08b464a22cdbbbc4e7e95192b1d0b7556
Cara Hill <cara@rabidtech.co.nz>
2018-11-29 17:01:17 +1300

    Broke all the things - WIP refactor.

--- secret-handshake.js
+++ secret-handshake.js
@@ -10,34 +10,52 @@ function secretHandshake(number) {
     1000: "jump"
   };
 
-  let binaryNumber = (+number).toString(2);
+  // let binaryNumber = (+number).toString(2);
   let handshake = [];
 
-  for(let key in secrets) {
-    if(binaryNumber == key) {
-      handshake.push(secrets[key]);
-    }
-  }
+  // for(let key in secrets) {
+  //   if(binaryNumber == key) {
+  //     handshake.push(secrets[key]);
+  //   }
+  // }
 
-  if(binaryNumber % 10 == 1 && binaryNumber != 1) {
-    let stringNumber = binaryNumber.toString();
-    if(stringNumber.endsWith("11")) {
-      handshake.push(secrets[1]);
-      handshake.push(secrets[10]);
-    }
+  // if(binaryNumber % 10 == 1 && binaryNumber != 1) {
+  //   let stringNumber = binaryNumber.toString();
+  //   if(stringNumber.endsWith("11")) {
+  //     handshake.push(secrets[1]);
+  //     handshake.push(secrets[10]);
+  //   }
 
-    if(stringNumber.endsWith("111")) {
-      handshake.push(secrets[100]);
-    }
+  //   if(stringNumber.endsWith("111")) {
+  //     handshake.push(secrets[100]);
+  //   }
 
-    if(stringNumber.endsWith("1111")) {
-      handshake.push(secrets[1000]);
-    }
-  }
+  //   if(stringNumber.endsWith("1111")) {
+  //     handshake.push(secrets[1000]);
+  //   }
+  // }
 
-  if(binaryNumber >= 10000) {
-    handshake.reverse();
-  }
+  // if(binaryNumber >= 10000) {
+  //   handshake.reverse();
+  // }
+
+  // return handshake;
+
+  let remainder;
+  let idx = 0;
+
+  do {
+    remainder = number % 2;
+    if (remainder === 1) {
+      if (idx === 4) {
+        handshake.reverse();
+      } else {
+        handshake.push(secrets[idx]);
+      }
+    }
+    idx += 1;
+    number = Math.floor(number / 2);
+  } while (number > 0);
 
   return handshake;
 }
