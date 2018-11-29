a8c60bb7d6066a21f9127bb39f4e397fda5868cd
Cara Hill <cara@rabidtech.co.nz>
2018-11-29 15:40:35 +1300

    Refactored the code for the first passing test.

--- secret-handshake.js
+++ secret-handshake.js
@@ -1,5 +1,20 @@
 function secretHandshake(number) {
-  return ["wink"];
+  const secrets = {
+    1: "wink",
+    10: "double blink",
+    100: "close your eyes",
+    1000: "jump"
+  };
+
+  let handshake = [];
+
+  for (let key in secrets) {
+    if(number == key) {
+      handshake.push(secrets[key]);
+    }
+  }
+
+  return handshake;
 }
 
 export { secretHandshake }
